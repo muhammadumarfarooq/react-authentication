0214118a1ea36153daedcdc7338f921a06c8cede
Muhammad Umar Farooq <develop.umar@gmail.com>
2020-04-15 12:29:42 +0500

    add code to GoogleAuth component to initialize the library

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -2,7 +2,13 @@ import React from "react";
 
 const GoogleAuth = () => {
   React.useEffect(() => {
-    window.gapi.load("client: auth2", () => {});
+    window.gapi.load("client: auth2", () => {
+      window.gapi.init({
+        clientId:
+          "54580018406-4lf8t1qo9qjk6lueppnqt130hfvqgqcl.apps.googleusercontent.com",
+        scope: "email",
+      });
+    });
 
     // exlint-disable-next-line
   }, []);
